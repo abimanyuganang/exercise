b201155c3da107eaa4abd165252a0e7fb3b682fb
ganangAbimanyu <lilmanyuuu@gmail.com>
2025-05-22 11:06:49 +0800

    excercise 11

--- src/main/java/excercise11/BankAccountMain.java
+++ src/main/java/excercise11/BankAccountMain.java
@@ -4,7 +4,6 @@ public class BankAccountMain {
     public static void main(String[] args) {
         BankAccountWithLock account = new BankAccountWithLock(1000.0);
 
-        // Reader thread
         Thread reader = new Thread(() -> {
             for (int i = 0; i < 3; i++) {
                 account.getBalance();
@@ -16,22 +15,18 @@ public class BankAccountMain {
             }
         }, "ReaderThread");
 
-        // Depositor thread
         Thread depositor = new Thread(() -> {
             account.deposit(500.0);
         }, "DepositorThread");
 
-        // Withdrawer thread
         Thread withdrawer = new Thread(() -> {
             account.withdraw(200.0);
         }, "WithdrawerThread");
 
-        // Start threads
         reader.start();
         depositor.start();
         withdrawer.start();
 
-        // Wait for all threads to complete
         try {
             reader.join();
             depositor.join();
