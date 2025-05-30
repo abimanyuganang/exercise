514b49f2f00474d474848be0ef66cf15cc697d63
ganangAbimanyu <lilmanyuuu@gmail.com>
2025-05-30 22:08:02 +0800

    changes line 9 new and week 10 packages

--- src/main/java/excercise11/BankAccountMain.java
+++ src/main/java/excercise11/BankAccountMain.java
@@ -6,7 +6,7 @@ public class BankAccountMain {
 
         Thread reader = new Thread(() -> {
             for (int i = 0; i < 3; i++) {
-                unt.getBalance();
+                account.getBalance();
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
